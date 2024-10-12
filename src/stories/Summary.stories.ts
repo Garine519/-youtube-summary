94d7688868c5867bbdcf51510fab493c3e5b9042
garine519 <garine-5-19@hotmail.com>
2024-10-12 12:57:20 -0400

    add Summary story

---
+++ src/stories/Summary.stories.ts
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import Summary from "../components/Summary";
+
+const meta = {
+  title: "Components/Summary",
+  component: Summary,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  args: { onSummaryFetch: fn() },
+} satisfies Meta<typeof Summary>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+export const Idle: Story = {};
+export const Unavailable: Story = {
+  args: {
+    unavailable: true,
+  },
+};
+
+export const Success: Story = {
+  args: {
+    data: 'This is a summary',
+  },
+};
+
+export const Error: Story = {
+  args: {
+    error: 'An error occurred. Status code: 500',
+  },
+};
