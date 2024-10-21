a8f468c12b5d0158d8b15a4127cf5df4f9457d33
garine519 <garine-5-19@hotmail.com>
2024-10-21 12:32:24 -0400

    add icons

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,10 @@ export default defineConfig({
         {
           src: 'public/manifest.json',
           dest: '.',
+        },
+        {
+          src: 'public/icons/*',
+          dest: './icons',
         }
       ],
     }),
